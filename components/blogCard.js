a5f9a132335db8d9a74b271b00c070a21a4fc802
Anuradha Kumari <2anuradha9@gmail.com>
2021-08-01 18:47:11 +0530

    added grid layout and link for medium blog

--- components/blogCard.js
+++ components/blogCard.js
@@ -5,32 +5,38 @@ const Blogcard = ({ posts, label }) => {
   return (
     <div className="py-2">
       <h1 className="py-4 text-3xl font-bold">{label}</h1>
-      {posts.map((post, index) => {
-        return (
-            <div key={post.id} className="max-w-3xl shadow-lg mb-8 rounded-lg mx-auto flex">
-              {/* <div
-                className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                style={{ backgroundImage: `url(${post.coverImage.url})` }}
-                title={post.title}
-              /> */}
-              <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div className="mb-4">
-                  <div className="text-gray-900 font-bold text-xl mb-2">
-                    <Link as={`/blog/${post.slug}`} href="/blog/[slug]">
-                      <a>{post.title}</a>
-                    </Link>
+      <div>
+        Note: I am working on migrating my blog posts to the website. Till that time, you can {' '}
+      <Link href="https://anuradhak.medium.com/"><a className="text-lg">check all my blogs on medium</a></Link>.
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 pt-8">
+        {posts.map((post) => {
+          return (
+              <div key={post.id} className="max-w-3xl shadow-lg mb-8 rounded-lg mx-auto flex">
+                {/* <div
+                  className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                  style={{ backgroundImage: `url(${post.coverImage.url})` }}
+                  title={post.title}
+                /> */}
+                <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                  <div className="mb-4">
+                    <div className="text-gray-900 font-bold text-xl mb-2">
+                      <Link as={`/blog/${post.slug}`} href="/blog/[slug]">
+                        <a>{post.title}</a>
+                      </Link>
+                    </div>
+                    <p className="text-gray-700 text-base">{post.excerpt}</p>
                   </div>
-                  <p className="text-gray-700 text-base">{post.excerpt}</p>
-                </div>
-                <div className="flex items-center">
-                  <div className="text-sm">
-                    <p className="text-gray-600">{post.published}</p>
+                  <div className="flex items-center">
+                    <div className="text-sm">
+                      <p className="text-gray-600">{post.published}</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -26,28 +26,25 @@ export async function getStaticProps() {
 
 export default function Home({ about, futureTalks, posts, mdDetails }) {
   return (
-    <section className="flex flex-wrap items-center justify-center max-w-2xl py-16 sm:py-28">
-      <div className="flex items-center justify-center">
+    <section className="flex flex-wrap items-center justify-center px-4 sm:px-10 lg:px-28 py-16 sm:py-28">
+      <div className="flex items-center justify-center w-full">
         <Image width="70" height="70" src={about.person.photo.url} alt="" className="rounded-full"  />
         <h1 className="px-4 text-3xl font-bold">{about.title}</h1> 
       </div>
-      <p className="text-center pt-8 text-lg">
+      <p className="text-center pt-8 text-lg w-3xl">
         {about.subtitle}
       </p>
-      <div className={styles['details-md']} dangerouslySetInnerHTML={{ __html:mdDetails }}></div>
+      <div className="w-2xl">
+        <div className={styles['details-md']} dangerouslySetInnerHTML={{ __html:mdDetails }}></div>
+      </div>
       <div className="py-12 w-full">
        <TalkCard talks={futureTalks} label="Upcoming speaking engagements" />
        <Link href="/talks"><a className="text-lg">All talks</a></Link>
       </div>
       <div>
         <BlogCard posts={posts} label={`Recent Blog posts`} />
-        <Link href="/blog"><a className="text-lg">All blog posts</a></Link>
+        <Link href="https://anuradhak.medium.com/"><a className="text-lg">All blog posts</a></Link>
       </div>
-      <p className="text-center py-20 text-xl">
-        <div>Note:</div>
-        I am in process of moving all my data to the website. But meanwhile, check my all talks and blogs at {' '}
-        <Link href="https://anuk79.netlify.app/">anuk79.netlify.com</Link>
-      </p>
     </section>
   );
 }
