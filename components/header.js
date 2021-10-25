834e8abb53b3be34c65f369666777c76aeb9ec95
Anuradha Kumari <2anuradha9@gmail.com>
2021-10-25 10:30:48 +0200

    fix: some seo changes

--- components/header.js
+++ components/header.js
@@ -32,8 +32,16 @@ function Header({ navLinks = [] }) {
         </nav>
         <Head>
           <title>Anuradha Kumari</title>
-          <meta name="description" content="Website of Anuradha Kumari" />
+          <meta name="description" content="My name is Anuradha Kumari. I am a frontend developer, making the web accessible, one website at a time." />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <meta name="og:title" content="Anuradha Kumari - a11y advocate, GDE for web, MDE" />
+          <meta name="og:description" content="Frontend developer, making the web accessible - one website at a time." />
+          <meta name="og:type" content="website" />
+          <meta name="og:image" content="https://www.anuradhakumari.com/og.png" />
         </Head>
         
     </header>
