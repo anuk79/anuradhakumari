bc302225ee26504c5ae44f6394eb8b9c57ae970f
Anuradha Kumari <2anuradha9@gmail.com>
2022-01-17 18:37:40 +0100

    fix: fetching til from graphcms

--- pages/til/[slug].js
+++ pages/til/[slug].js
@@ -6,32 +6,32 @@ import markdownToHtml from '../../utils/markdownToHtml';
 import styles from '../../styles/Home.module.css';
 
 export async function getStaticProps({ params }) {
-    const pageDetails = await getPageDetails();
-    const { post } = await getBlogPost(params.slug);
-    const mdContent = await markdownToHtml(post.content || '')
-    return {
-        props: {
-          ...pageDetails,
-          post: {...post, mdContent},
-        },
-    };
+  const pageDetails = await getPageDetails();
+  const { post } = await getBlogPost(params.slug);
+  const mdContent = await markdownToHtml(post.content || '');
+  return {
+    props: {
+      ...pageDetails,
+      post: { ...post, mdContent },
+    },
+  };
 }
 
 export async function getStaticPaths() {
-    const posts = await getBlogs();
-    return {
-        paths: Object.values(posts).map(({ slug }) => ({
-        params: { slug },
-        })),
-        fallback: false,
-    };
+  const posts = await getBlogs('TIL');
+  return {
+    paths: Object.values(posts).map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
 }
 
 const Post = ({ post }) => {
   return (
     <div className="py-2 min-h-screen max-w-4xl w-full">
       <Head>
-        <title>{post.title} - Anuradha&apos;s blog</title>
+        <title>{post.title} - Anuradha Kumari</title>
       </Head>
       <div className="head-section">
         <h1 className="text-3xl md:text-4xl py-4 md:px-8">
@@ -54,7 +54,7 @@ const Post = ({ post }) => {
           <div className={styles['markdown']} dangerouslySetInnerHTML={{ __html: post.mdContent }}></div>
         </div>
       </div>
-      
+
       <div className="mx-auto mb-12">
         <Link href="/til">
           <a className="buttoned">All code tips</a>
