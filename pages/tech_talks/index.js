32ab220333e6a0ccb315328093fdea7c05f27e9e
Anuradha Kumari <2anuradha9@gmail.com>
2022-08-19 14:36:11 +0200

    Update index.js

--- pages/tech_talks/index.js
+++ pages/tech_talks/index.js
@@ -5,55 +5,52 @@ import getPageDetails from '../../queries/getPageDetails';
 import { getTalkDetails } from '../../queries/getTalkDetails';
 
 export async function getStaticProps() {
-    const pageDetails = await getPageDetails();
-    const talks = await getTalkDetails();
-    const futureTalks = [], pastTalks = [];
-    talks.talks.forEach(talk => {
-      if (talk.talkStatus === 'past') {
-        pastTalks.push(talk);
-      } else {
-        futureTalks.push(talk);
-      }
-    });
-  
-    return {
-      props: {
-        ...pageDetails,
-        futureTalks,
-        pastTalks
-      },
-    };
-  }
+  const pageDetails = await getPageDetails();
+  const talkDetails = await getTalkDetails();
+  // const topics = [...new Set(talkDetails.futureTalks.map(({ topics }) => topics).flat()),
+  // ...new Set(talkDetails.pastTalks.map(({ topics }) => topics).flat())];
 
-const Talks = ({ pastTalks = [], futureTalks = [] }) => {
+  return {
+    props: {
+      ...pageDetails,
+      futureTalks: talkDetails.futureTalks,
+      pastTalks: talkDetails.pastTalks,
+      // topics
+    },
+  };
+}
+
+const Talks = ({ pastTalks = [], futureTalks = [], }) => {
   return (
     <div className="pb-8 px-1 sm:px-4 max-w-4xl">
       <Head>
         <title>Talks - Anuradha Kumari</title>
       </Head>
-      <div className="head-section">
-        <h1 className="text-3xl pt-4 font-bold">Tech talks</h1>
-        <div className="py-4">
-          I am passionate about exploring new technologies and sharing the knowledge and experience with the community. 
+      <section>
+        <h1 className="text-4xl py-4 md:pt-12 font-bold">Tech talks</h1>
+        <p className="py-4 text-lg">
+          I am passionate about exploring new technologies and sharing the knowledge and experience with the community.
           I love the technical discussions and they motivate me to learn and explore further.
+        </p>
+      </section>
+      <section>
+        <h2 className="text-3xl pt-4 pb-2 font-bold">Currently accepting speaking invitations</h2>
+        <div className="py-4 text-lg">
+          If you’d like to invite me to speak at your event,
+          please <a href="mailto:contact@anuradhakumari.com">send me an email</a>.
         </div>
-      </div>
-      <div>
-        <h2 className="text-3xl pt-4 font-bold">Currently accepting speaking invitations</h2>
-        <div className="py-4">
-          If you’d like to invite me to speak at your event, please {' '} 
-          <a href="mailto:contact@anuradhakumari.com">send me an email</a>.
-          <br />
-          <br />
-          Feedbacks help in improvements. If you would like to give feedback for any of my talks, {' '}
-          <Link href="https://forms.gle/DWntc1phDUPrAZBm7">
+        <div className="pb-8 text-lg">
+          Feedbacks help in improvements. If you would like to give feedback for any of my
+          talks, <Link href="https://forms.gle/DWntc1phDUPrAZBm7">
             <a>please fill the feedback form</a>
           </Link>
-           .
         </div>
-      </div>
-      <TalkCard talks={futureTalks} label={`Upcoming sessions (${futureTalks.length})`} />
-      <TalkCard talks={pastTalks} label={`Past sessions (${pastTalks.length})`} />
+      </section>
+      <section>
+
+      </section>
+      <TalkCard talks={futureTalks} label={`Upcoming events (${futureTalks.length})`} />
+      <TalkCard talks={pastTalks} label={`Past events (${pastTalks.length})`} />
     </div>
   );
 };
