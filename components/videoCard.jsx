671958ecaf669c56e421a09556d12fba4afe6f60
Anuradha Kumari <2anuradha9@gmail.com>
2022-08-19 13:41:12 +0200

    fix image issue

--- components/videoCard.jsx
+++ components/videoCard.jsx
@@ -6,16 +6,18 @@ const ImageCard = ({ data, label, headerTag }) => {
     return (
         <div className="py-2">
             <HeadingTag className="py-4 text-3xl font-bold">{label}</HeadingTag>
-            <div className="grid grid-cols-1">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                 {data?.map((detail) => {
                     return (
-                        <div key={detail.id} className="w-full mx-auto flex">
+                        detail?.link?.url && (<div key={detail.id} className="w-full mx-auto flex">
                             <div className="py-4 flex flex-col justify-between leading-normal">
-                                <Link href={detail?.link?.url || '/'}>
-                                    <a>{detail?.link?.displayText}</a>
+                                <Link href={detail.link.url}>
+                                    <a aria-label={detail.link.displayText}>
+                                        <Image width="280" height="170" src={detail.coverImage?.url} alt="" />
+                                    </a>
                                 </Link>
                             </div>
-                        </div>
+                        </div>)
                     );
                 })}
             </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['media.graphcms.com']
+    domains: ['media.graphcms.com', 'media.graphassets.com']
   },
   i18n: {
     locales: ["en"],
