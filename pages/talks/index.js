c163617deddf397d742c45c9b1468fe507147a84
Anuradha Kumari <2anuradha9@gmail.com>
2022-08-19 14:21:52 +0200

    sorted future talks by date ascending, added new books and articles read

--- pages/talks/index.js
+++ pages/talks/index.js
@@ -6,39 +6,34 @@ import { getTalkDetails } from '../../queries/getTalkDetails';
 
 export async function getStaticProps() {
   const pageDetails = await getPageDetails();
-  const talks = await getTalkDetails();
-  const futureTalks = [], pastTalks = [];
-  talks.talks.forEach(talk => {
-    if (talk.talkStatus === 'past') {
-      pastTalks.push(talk);
-    } else {
-      futureTalks.push(talk);
-    }
-  });
+  const talkDetails = await getTalkDetails();
+  // const topics = [...new Set(talkDetails.futureTalks.map(({ topics }) => topics).flat()),
+  // ...new Set(talkDetails.pastTalks.map(({ topics }) => topics).flat())];
 
   return {
     props: {
       ...pageDetails,
-      futureTalks,
-      pastTalks
+      futureTalks: talkDetails.futureTalks,
+      pastTalks: talkDetails.pastTalks,
+      // topics
     },
   };
 }
 
-const Talks = ({ pastTalks = [], futureTalks = [] }) => {
+const Talks = ({ pastTalks = [], futureTalks = [], }) => {
   return (
     <div className="pb-8 px-1 sm:px-4 max-w-4xl">
       <Head>
         <title>Talks - Anuradha Kumari</title>
       </Head>
-      <div className="">
+      <section>
         <h1 className="text-4xl py-4 md:pt-12 font-bold">Tech talks</h1>
         <p className="py-4 text-lg">
           I am passionate about exploring new technologies and sharing the knowledge and experience with the community.
           I love the technical discussions and they motivate me to learn and explore further.
         </p>
-      </div>
-      <div>
+      </section>
+      <section>
         <h2 className="text-3xl pt-4 pb-2 font-bold">Currently accepting speaking invitations</h2>
         <div className="py-4 text-lg">
           If you’d like to invite me to speak at your event,
@@ -50,9 +45,12 @@ const Talks = ({ pastTalks = [], futureTalks = [] }) => {
             <a>please fill the feedback form</a>
           </Link>
         </div>
-      </div>
-      <TalkCard talks={futureTalks} label={`Upcoming sessions (${futureTalks.length})`} />
-      <TalkCard talks={pastTalks} label={`Past sessions (${pastTalks.length})`} />
+      </section>
+      <section>
+
+      </section>
+      <TalkCard talks={futureTalks} label={`Upcoming events (${futureTalks.length})`} />
+      <TalkCard talks={pastTalks} label={`Past events (${pastTalks.length})`} />
     </div>
   );
 };
--- queries/getTalkDetails.js
+++ queries/getTalkDetails.js
@@ -3,10 +3,18 @@ import { GraphQLClient } from 'graphql-request';
 const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
 
 export async function getTalkDetails() {
-    const { talks } = await graphcms.request(
+    const { futureTalks, pastTalks } = await graphcms.request(
         `
             query {
-                talks(orderBy: date_DESC) {
+                futureTalks: talks(where: {talkStatus: future},orderBy: date_ASC) {
+                    id
+                    topic
+                    talkStatus
+                    topics
+                    date
+                    eventDetails
+                }
+                pastTalks: talks(where: {talkStatus: past}, orderBy: date_DESC) {
                     id
                     topic
                     talkStatus
@@ -19,7 +27,8 @@ export async function getTalkDetails() {
     );
   
     return {
-        talks
+        futureTalks,
+        pastTalks,
     }
   }
 
@@ -27,7 +36,7 @@ export async function getFutureTalkDetails() {
     const { talks } = await graphcms.request(
         `
             query {
-                talks(where: {talkStatus: future}) {
+                talks(where: {talkStatus: future}, orderBy: date_ASC) {
                     id
                     topic
                     talkStatus
