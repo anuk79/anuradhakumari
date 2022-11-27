8cd9ed844fcf7edb42061495608eb9970e72f118
Anuradha Kumari <2anuradha9@gmail.com>
2022-11-27 23:43:15 +0100

    comment talk queries

--- queries/getTalkDetails.js
+++ queries/getTalkDetails.js
@@ -1,54 +1,54 @@
-import { GraphQLClient } from 'graphql-request';
+// import { GraphQLClient } from 'graphql-request';
 
-const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
+// const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
 
-export async function getTalkDetails() {
-    const { futureTalks, pastTalks } = await graphcms.request(
-        `
-            query {
-                futureTalks: talks(where: {talkStatus: future},orderBy: date_ASC) {
-                    id
-                    topic
-                    talkStatus
-                    topics
-                    date
-                    eventDetails
-                }
-                pastTalks: talks(where: {talkStatus: past}, orderBy: date_DESC) {
-                    id
-                    topic
-                    talkStatus
-                    topics
-                    date
-                    eventDetails
-                }
-            }
-        `
-    );
+// export async function getTalkDetails() {
+//     const { futureTalks, pastTalks } = await graphcms.request(
+//         `
+//             query {
+//                 futureTalks: talks(where: {talkStatus: future},orderBy: date_ASC) {
+//                     id
+//                     topic
+//                     talkStatus
+//                     topics
+//                     date
+//                     eventDetails
+//                 }
+//                 pastTalks: talks(where: {talkStatus: past}, orderBy: date_DESC) {
+//                     id
+//                     topic
+//                     talkStatus
+//                     topics
+//                     date
+//                     eventDetails
+//                 }
+//             }
+//         `
+//     );
   
-    return {
-        futureTalks,
-        pastTalks,
-    }
-  }
+//     return {
+//         futureTalks,
+//         pastTalks,
+//     }
+//   }
 
-export async function getFutureTalkDetails() {
-    const { talks } = await graphcms.request(
-        `
-            query {
-                talks(where: {talkStatus: future}, orderBy: date_ASC) {
-                    id
-                    topic
-                    talkStatus
-                    topics
-                    date
-                    eventDetails
-                }
-            }
-        `
-    );
+// export async function getFutureTalkDetails() {
+//     const { talks } = await graphcms.request(
+//         `
+//             query {
+//                 talks(where: {talkStatus: future}, orderBy: date_ASC) {
+//                     id
+//                     topic
+//                     talkStatus
+//                     topics
+//                     date
+//                     eventDetails
+//                 }
+//             }
+//         `
+//     );
 
-    return {
-        futureTalks: talks
-    }
-}
+//     return {
+//         futureTalks: talks
+//     }
+// }
